296509750b3f15b4f47537bf4fa0235dc2b633d5
libterty <dreak603@gmail.com>
2020-06-04 14:07:18 +0800

    chore: change k8s conifg

--- k8s.sh
+++ k8s.sh
@@ -3,70 +3,79 @@
 set -x
 
 auto_start_k8s() {
-  files=($(find -E . -type f -regex "^.*.yaml$"))
-  for item in ${files[*]}; do
-    kubectl apply -f $item
-  done
+    files=($(find -E . -type f -regex "^.*.yaml$"))
+    for item in ${files[*]}; do
+        kubectl apply -f $item
+    done
 }
 
 write_srv_node_port_k8s() {
-  # $2 stands for the file name of your yaml
-  touch $2-srv.yaml
 
-  #3 stands for port
-  #4 stands for targetPort
-  echo "apiVersion: v1" >>$2-srv.yaml
-  echo "kind: Service" >>$2-srv.yaml
-  echo "metadata:" >>$2-srv.yaml
-  echo "  name: $2-srv" >>$2-srv.yaml
-  echo "spec:" >>$2-srv.yaml
-  echo "  type: NodePort" >>$2-srv.yaml
-  echo "  selector:" >>$2-srv.yaml
-  echo "    app: $2" >>$2-srv.yaml
-  echo "  ports:" >>$2-srv.yaml
-  echo "    - name: $2" >>$2-srv.yaml
-  echo "      protocol: TCP" >>$2-srv.yaml
-  echo "      port: $3" >>$2-srv.yaml
-  echo "      targetPort: $4" >>$2-srv.yaml
+    fileName=$2
+    port=$3
+    targetPort=$4
+    # $2 stands for the file name of your yaml
+    touch "$fileName-srv.yaml"
+
+    #3 stands for port
+    #4 stands for targetPort
+    echo "apiVersion: v1" >>"$fileName-srv.yaml"
+    echo "kind: Service" >>"$fileName-srv.yaml"
+    echo "metadata:" >>"$fileName-srv.yaml"
+    echo "  name: $fileName-srv" >>"$fileName-srv.yaml"
+    echo "spec:" >>"$fileName-srv.yaml"
+    echo "  type: NodePort" >>"$fileName-srv.yaml"
+    echo "  selector:" >>"$fileName-srv.yaml"
+    echo "    app: $fileName" >>"$fileName-srv.yaml"
+    echo "  ports:" >>"$fileName-srv.yaml"
+    echo "    - name: $fileName" >>"$fileName-srv.yaml"
+    echo "      protocol: TCP" >>"$fileName-srv.yaml"
+    echo "      port: $port" >>"$fileName-srv.yaml"
+    echo "      targetPort: $targetPort" >>"$fileName-srv.yaml"
 }
 
 write_depl_k8s() {
-  # $2 stands for the file name of your yaml
-  touch $2-depl.yaml
+    fileName=$2
+    replicas=$3
+    user=$4
+    # $2 stands for the file name of your yaml
+    touch $fileName-depl.yaml
 
-  #3 stands for port
-  #4 stands for targetPort
-  echo "apiVersion: v1" >>$2-depl.yaml
-  echo "kind: Service" >>$2-depl.yaml
-  echo "metadata:" >>$2-depl.yaml
-  echo "  name: $2-depl" >>$2-depl.yaml
-  echo "spec:" >>$2-depl.yaml
-  echo "  replicas: $3" >>$2-depl.yaml
-  echo "  selector:" >>$2-depl.yaml
-  echo "    matchLabels:" >>$2-depl.yaml
-  echo "      app: $2" >>$2-depl.yaml
-  echo "  template:" >>$2-depl.yaml
-  echo "    metadata:" >>$2-depl.yaml
-  echo "      labels:" >>$2-depl.yaml
-  echo "      app: $2" >>$2-depl.yaml
-  echo "    spec:" >>$2-depl.yaml
-  echo "      containers:" >>$2-depl.yaml
-  echo "        - name: $2" >>$2-depl.yaml
-  echo "          image: $4/$2:latest" >>$2-depl.yaml
-  echo "          resources:" >>$2-depl.yaml
-  echo "            limits:" >>$2-depl.yaml
-  echo "              memory: "128Mi"" >>$2-depl.yaml
-  echo "              cpu: "500m"" >>$2-depl.yaml
+    #3 stands for port
+    #4 stands for targetPort
+    echo "apiVersion: v1" >>$fileName-depl.yaml
+    echo "kind: Service" >>$fileName-depl.yaml
+    echo "metadata:" >>$fileName-depl.yaml
+    echo "  name: $fileName-depl" >>$fileName-depl.yaml
+    echo "spec:" >>$fileName-depl.yaml
+    echo "  replicas: $replicas" >>$fileName-depl.yaml
+    echo "  selector:" >>$fileName-depl.yaml
+    echo "    matchLabels:" >>$fileName-depl.yaml
+    echo "      app: $fileName" >>$fileName-depl.yaml
+    echo "  template:" >>$fileName-depl.yaml
+    echo "    metadata:" >>$fileName-depl.yaml
+    echo "      labels:" >>$fileName-depl.yaml
+    echo "      app: $fileName" >>$fileName-depl.yaml
+    echo "    spec:" >>$fileName-depl.yaml
+    echo "      containers:" >>$fileName-depl.yaml
+    echo "        - name: $fileName" >>$fileName-depl.yaml
+    echo "          image: $user/$fileName:latest" >>$fileName-depl.yaml
+    echo "          resources:" >>$fileName-depl.yaml
+    echo "            limits:" >>$fileName-depl.yaml
+    echo "              memory: "128Mi"" >>$fileName-depl.yaml
+    echo "              cpu: "500m"" >>$fileName-depl.yaml
 }
 
 main() {
-  if [ "$1" = "start" ]; then
-    auto_start_k8s
-  elif [ "$1" = "srv" ]; then
-    write_srv_node_port_k8s
-  elif [ "$1" = "depl" ]; then
-    write_depl_k8s
-  fi
+    if [ $1 = start ]; then
+        auto_start_k8s
+    elif [ $1 = srv ]; then
+        write_srv_node_port_k8s $1 $2 $3 $4
+    elif [ $1 = depl ]; then
+        write_depl_k8s $1 $2 $3 $4
+    else
+        echo Fail with Wrong arg
+    fi
 }
 
-main
+main $1 $2 $3 $4
